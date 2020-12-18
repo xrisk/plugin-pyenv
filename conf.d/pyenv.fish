2d50d6496ad259110298cd4123738925e01ca4b5
Rishav Kundu <rk@rishav.io>
2020-12-19 03:48:15 +0530

    Try adding pyenv-virtualenv config

--- conf.d/pyenv.fish
+++ conf.d/pyenv.fish
@@ -1,4 +1,4 @@
 set -q PYENV_ROOT; or set -l PYENV_ROOT $HOME/.pyenv
 
-set PATH $PYENV_ROOT/libexec/pyenv $PYENV_ROOT/libexec $PYENV_ROOT/shims $PYENV_ROOT/bin $PATH 2> /dev/null
+set -gx PATH $PYENV_ROOT/shims $PATH
 setenv PYENV_SHELL fish
